29cecaf803554a0275fb8d82b2503a9492728a9c
hermann <adinsihermann1@gmail.com>
2021-06-13 23:03:42 +0200

    dépot quete routing SpringBoot

---
+++ src/main/java/Doctors/quete1/Quete1Application.java
@@ -0,0 +1,13 @@
+package Doctors.quete1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quete1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quete1Application.class, args);
+	}
+
+}
---
+++ src/test/java/Doctors/quete1/Quete1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Doctors.quete1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quete1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
